c3fc848f382fff3e8151887159b8b719d82aba85
samo <janezic.orion@gmail.com>
2023-04-19 16:27:04 +0200

    All working to second layout.  YT timestamp 1:30:00

---
+++ views/login.php
@@ -0,0 +1,14 @@
+<h1>Contact us!</h1>
+
+<form action="" method="post">
+  <div class="mb-3">
+    <label>E-mail</label>
+    <input type="text" name="email" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label>Password</label>
+    <input name="password" class="form-control"></input>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ views/register.php
@@ -0,0 +1,27 @@
+<h1>Create an account</h1>
+
+<form action="" method="post">
+  <div class="row">
+    <div class="mb-3 col">
+      <label>First name</label>
+      <input type="text" name="firstname" class="form-control">
+    </div>
+    <div class="mb-3 col">
+      <label>Last name</label>
+      <input type="text" name="lastname" class="form-control">
+    </div>
+  </div>
+  <div class="mb-3">
+    <label>E-mail</label>
+    <input type="text" name="email" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label>Password</label>
+    <input type="password" name="password" class="form-control"></input>
+  </div>
+  <div class="mb-3">
+    <label>Confirm password</label>
+    <input type="password" name="confirmPassword" class="form-control"></input>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
