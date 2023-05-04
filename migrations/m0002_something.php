51d3a8c04d42bc01aed4e9a325574c9c64c4fce2
samo <janezic.orion@gmail.com>
2023-05-04 13:29:46 +0200

    Had problems with db, now everything works as expected

---
+++ migrations/m0002_something.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0002_something
+{
+  public function up()
+  {
+    echo 'applying migration';
+  }
+
+  public function down()
+  {
+    echo 'Down migration';
+  }
+}
--- public/index.php
+++ public/index.php
@@ -4,13 +4,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 use \app\controllers\SiteController;
 use \app\controllers\AuthController;
 use app\core\Application;
 
-$app = new Application(dirname(__DIR__));
+require_once __DIR__.'/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+  'db' => [
+    'dsn' => $_ENV['DB_DSN'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
+  ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
