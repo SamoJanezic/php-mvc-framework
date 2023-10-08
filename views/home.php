cda80bf4e70b93cc70bbc86112ada77e8c11ac83
SamoJanezic <janezic.orion@gmail.com>
2023-10-08 16:55:54 +0200

    added feature to show all posts on home page

--- views/home.php
+++ views/home.php
@@ -1,2 +1,22 @@
+<?php
+  use samojanezic\phpmvc\Application;
+  use app\models\Post;
+?>
+
 <h1>Home</h1>
-<h3>Welcome to <?php echo $name ?></h3>
+<h3>Welcome to <? echo $name ?></h3>
+
+
+<?
+
+$post = new Post;
+$allPosts = $post->getAllPosts();
+
+forEach($allPosts as $single) {
+?>
+<div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
+<div>on:            <? echo $single->created_on ?></div>
+<img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail' style= width:200px; float:left>
+<h5>                <? echo $single->title ?></h5>
+<div>               <? echo substr($single->content, 0, 100) ?>... <a href="">[read more]</a></div>
+<? } ?>
