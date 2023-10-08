5ac16cf0deaa3d952418789f2be13d76f61f3758
SamoJanezic <janezic.orion@gmail.com>
2023-10-08 16:51:10 +0200

    added field for image

--- views/create.php
+++ views/create.php
@@ -1,7 +1,5 @@
 <?php
 use samojanezic\phpmvc\form\TextareaField;
-
-$this->title = 'Create';
 ?>
 
 <h1>Create new blog</h1>
@@ -9,5 +7,6 @@ $this->title = 'Create';
 <?php $form = \samojanezic\phpmvc\form\Form::begin('', 'post') ?>
 <?php echo $form->field($model, 'title') ?>
 <?php echo new TextareaField($model, 'body') ?>
+<?php echo $form->field($model, 'image') ?>
 <button type="submit" class="btn btn-primary">Save</button>
 <?php \samojanezic\phpmvc\form\Form::end(); ?>
