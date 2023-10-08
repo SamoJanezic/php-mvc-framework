0867f08f94a77eb9fa86d2ca5c72ad977491e366
SamoJanezic <janezic.orion@gmail.com>
2023-10-08 16:53:34 +0200

    added field for image

--- models/CreateForm.php
+++ models/CreateForm.php
@@ -8,20 +8,23 @@ class CreateForm extends Model
 {
     public string $title = '';
     public string $body = '';
+    public string $image = '';
 
     public function rules(): array
     {
         return [
             'title' => [self::RULE_REQUIRED],
             'body' => [self::RULE_REQUIRED],
+            'image' => [self::RULE_REQUIRED]
         ];
     }
 
     public function labels(): array
     {
         return [
-            'title' => 'Enter your title',
-            'body' => 'Enter Your body',
+            'title' => 'Enter post title',
+            'body' => 'Enter post body',
+            'image' => 'Enter image link'
         ];
     }
 
