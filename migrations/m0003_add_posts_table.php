66bb0394c2e45edd85c0b66ad8722df05a3e7e1b
SamoJanezic <janezic.orion@gmail.com>
2023-11-03 16:17:48 +0100

    added posts to migrations

---
+++ migrations/m0003_add_posts_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use samojanezic\phpmvc\Application;
+class m0003_add_posts_table
+{
+  public function up()
+  {
+    $db = Application::$app->db;
+    $SQL = "CREATE TABLE posts (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        content VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      ) ENGINE=INNODB;";
+    $db->pdo->exec($SQL);
+  }
+
+  public function down()
+  {
+    $db = Application::$app->db;
+    $SQL = "DROP TABLE posts;";
+    $db->pdo->exec($SQL);
+  }
+}
