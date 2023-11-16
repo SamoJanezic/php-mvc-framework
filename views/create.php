580a1d38d2807690181a25dc8a0a466c51ff07a9
SamoJanezic <janezic.orion@gmail.com>
2023-11-16 12:39:00 +0100

    Changed createForm to unified post

--- views/create.php
+++ views/create.php
@@ -1,14 +1,16 @@
 <?php
 /** @var $model \app\models\user */
-
+use samojanezic\phpmvc\form\Form;
 use samojanezic\phpmvc\form\TextareaField;
 ?>
 
 <h1>Create new blog</h1>
 
-<?php $form = \samojanezic\phpmvc\form\Form::begin('', 'post') ?>
-    <?php echo $form->field($model, 'title') ?>
-    <?php echo new TextareaField($model, 'content') ?>
-    <?php echo $form->field($model, 'image') ?>
+<?
+    $form = Form::begin('', 'post');
+    echo $form->field($model, 'title');
+    echo new TextareaField($model, 'content');
+    echo $form->field($model, 'image');
+?>
     <button type="submit" class="btn btn-primary">Submit</button>
-<?php \samojanezic\phpmvc\form\Form::end(); ?>
+<?  $form::end(); ?>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,7 +13,6 @@
   <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -60,7 +59,7 @@
   <div class="alert alert success">
     <?php echo Application::$app->session->getFlash('success'); ?>
   </div>
-  
+
   <?php endif; ?>
   {{content}}
 </div>
