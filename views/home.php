130ba04abb9b78625d2eab327983667dff66ce92
SamoJanezic <janezic.orion@gmail.com>
2023-11-16 12:35:03 +0100

    bigger teaser for first post

--- views/home.php
+++ views/home.php
@@ -10,16 +10,24 @@
   <?
   $post = new Post;
   $allPosts = $post->getAllPosts();
-  forEach($allPosts as $single) {
+  forEach($allPosts as $idx=>$single) {
   ?>
-
-  <div class="postBox">
-    <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
-    <div>on:            <? echo $single->created_on ?></div>
-    <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
-    <h5>                <? echo $single->title ?></h5>
-    <div>               <? echo substr($single->content, 0, 100) ?>... <a href="">[read more]</a></div>
-  </div>
-  <? } ?>
+    <? if($idx === 0) { ?>
+      <div class="firstPost">
+      <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
+      <div>on:            <? echo $single->created_on ?></div>
+      <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
+      <h5>                <? echo $single->title ?></h5>
+      <div>               <? echo substr($single->content, 0, 100) ?>... <a href="">[read more]</a></div>
+      </div>
+    <? } else { ?>
+    <div class="postBox">
+      <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
+      <div>on:            <? echo $single->created_on ?></div>
+      <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
+      <h5>                <? echo $single->title ?></h5>
+      <div>               <? echo substr($single->content, 0, 100) ?>... <a href="">[read more]</a></div>
+    </div>
+  <? }} ?>
 
 </div>
