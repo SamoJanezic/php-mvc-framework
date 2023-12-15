5492a056acd821cf46061cfc16d3ae3fbe048509
SamoJanezic <janezic.orion@gmail.com>
2023-12-16 00:29:21 +0100

    displays base info of user

--- views/profile.php
+++ views/profile.php
@@ -1,8 +1,31 @@
+
 <style><?php include 'css/profile.css'; ?></style>
 <?php
+use samojanezic\phpmvc\Application;
+
+$user = Application::$app->user;
+
+echo $user->getDisplayName();
+
+
 
 $this->title = 'Profile';
 
 ?>
 
 <h1>Profile</h1>
+
+<div class="info-box">
+    <span class="em">
+        <span>Email</span>
+        <span><?echo $user->email?></span>
+    </span>
+    <span class="em">
+        <span>First name</span>
+        <span><?echo $user->firstname?></span>
+    </span>
+    <span class="em">
+        <span>Last name</span>
+        <span><?echo $user->lastname?></span>
+    </span>
+</div>
