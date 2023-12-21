fe98d1bd46d8b9827207f23f145018e8597d4afc
SamoJanezic <janezic.orion@gmail.com>
2023-12-21 17:46:54 +0100

    made whole element in home clickable

--- views/home.php
+++ views/home.php
@@ -14,30 +14,30 @@
   forEach($allPosts as $idx=>$single) {
   ?>
   <? if($idx === 0) { ?>
-    <div  class="firstPost">
-      <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
-      <div>on:            <? echo $single->created_on ?></div>
-      <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
-      <h5>                <? echo $single->title ?></h5>
-      <div>               <? echo substr($single->content, 0, 100) ?>...
-        <form action="/postPage" method="get">
-          <input type="hidden" name="url_name" value="<? echo $single->url_name; ?>">
-          <button type="submit" class= "btn btn-link">[read more]</button>
-        </form>
-      </div>
+    <div class= "firstPost">
+      <form action="/postPage" method="get">
+        <input type="hidden" name="url_name" value="<? echo $single->url_name; ?>">
+        <button type="submit">
+          <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
+          <div>on:            <? echo $single->created_on ?></div>
+          <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
+          <h5>                <? echo $single->title ?></h5>
+          <div>               <? echo substr($single->content, 0, 100) ?>...</div>
+        </button>
+      </form>
     </div>
   <? } else { ?>
-    <div class="postBox">
-      <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
-      <div>on:            <? echo $single->created_on ?></div>
-      <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
-      <h5>                <? echo $single->title ?></h5>
-      <div>               <? echo substr($single->content, 0, 100) ?>...
-        <form action="/postPage" method="get">
-          <input type="hidden" name="url_name" value="<? echo $single->url_name; ?>">
-          <button type="submit" class= "btn btn-link">[read more]</button>
-        </form>
-      </div>
+    <div class= "postBox">
+      <form action="/postPage" method="get">
+        <input type="hidden" name="url_name" value="<? echo $single->url_name; ?>">
+        <button type="submit">
+          <div>published by:  <? echo $post->getPublisher($single->user_id) ?></div>
+          <div>on:            <? echo $single->created_on ?></div>
+          <img src=           <? echo $single->image ?> alt= 'no image found' class='img-thumbnail'>
+          <h5>                <? echo $single->title ?></h5>
+          <div>               <? echo substr($single->content, 0, 100) ?>...</div>
+        </button>
+      </form>
     </div>
   <? }} ?>
 </div>
