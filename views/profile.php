263681d835f41c762e7b3c5344d88b2c605ceb8c
SamoJanezic <janezic.orion@gmail.com>
2023-12-25 22:13:38 +0100

    show user image

--- views/profile.php
+++ views/profile.php
@@ -5,27 +5,30 @@ use samojanezic\phpmvc\Application;
 
 $user = Application::$app->user;
 
-echo $user->getDisplayName();
-
-
-
-$this->title = 'Profile';
-
 ?>
 
 <h1>Profile</h1>
 
 <div class="info-box">
-    <span class="em">
-        <span>Email</span>
-        <span><?echo $user->email?></span>
-    </span>
-    <span class="em">
-        <span>First name</span>
-        <span><?echo $user->firstname?></span>
-    </span>
-    <span class="em">
-        <span>Last name</span>
-        <span><?echo $user->lastname?></span>
-    </span>
+    <div>
+        <span class="em">
+            <span>Email</span>
+            <span><? echo $user->email ?? false;?></span>
+        </span>
+        <span class="em">
+            <span>First name</span>
+            <span><? echo $user->firstname?></span>
+        </span>
+        <span class="em">
+            <span>Last name</span>
+            <span><? echo $user->lastname?></span>
+        </span>
+        <span class="em">
+            <span>Password</span>
+            <span>reset password</span>
+        </span>
+    </div>
+    <div class="image-box">
+        <img src=<? echo $user->user_pic ?> alt="placeholder" class="user-pic">
+    </div>
 </div>
