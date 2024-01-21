92d205a23fe77024fdd01dd11bef87e7b31ffdb5
SamoJanezic <janezic.orion@gmail.com>
2024-01-21 23:20:34 +0100

    added methods to Vue

--- views/script/script.js
+++
@@ -1,5 +0,0 @@
-
-const app = Vue.createApp({
-    template:"<h2> hello </h2>"
-})
-app.mount("#app");
---
+++ views/script/script.vue
@@ -0,0 +1,18 @@
+
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'BloJist'
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.title = 'BloJist new'
+        }
+    },
+    template:`
+    <h2>{{ title }}</h2>
+    <button @click="changeTitle" class="btn btn-primary">click me</button>
+    `,
+})
+app.mount("#app");
