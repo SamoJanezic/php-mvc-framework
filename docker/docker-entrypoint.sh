23855dba312904381bdbe4fa9a6697de1ee55467
SamoJanezic <janezic.orion@gmail.com>
2024-01-31 01:35:08 +0100

    enrtrypoint test log removed

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp .env.example .env
 composer update
-./docker/wait-for-it.sh db:3306 --timeout=60 -- php migrations.php > testecho.txt
+./docker/wait-for-it.sh db:3306 --timeout=60 -- php migrations.php
 
 exec "$@"
